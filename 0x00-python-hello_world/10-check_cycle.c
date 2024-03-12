7b90227ebc8cbcf297fc49e183206730957ddbdc
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-12 16:08:22 -0700

    updated

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,10 +10,13 @@ int check_cycle(listint_t *list)
 {
 	listint_t *slow, *fast;
 
-	if (list == ! NULL || list->next == NULL)
+	if (list == NULL || list->next == NULL)
 	{
 		return (0);
 	}
+
+	listint_t *slow = list;
+	listint_t *fast = list->next;
 
 	while (slow && fast && fast->next)
 	{
