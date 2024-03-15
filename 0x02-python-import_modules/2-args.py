778556e5c79e3507986114776230b116ae8c119c
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-15 09:35:53 -0700

    Updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,11 @@ if __name__ == "__main__":
     """script to print the args"""
     from sys import argv
     x = len(argv) - 1
-    print("{}: argument{:s}".format(x, "s" if x != 1 else ""), end="")
-    if x == 0:
-        print(".")
+    if x < 1:
+        print("{} arguments.".format(x))
+    elif x == 1:
+        print("{} argument:".format(x))
     else:
-        print(":")
-        for i in range(x):
-            print("{}: {:s}".format(i + 1, argv[i + 1]))
+        print("{} arguments:".format(x))
+    for i in range(x):
+        print("{}: {:s}".format(i + 1, argv[i + 1]))
