ce8c72d05d7f45d569b2075ea6767e37013219b2
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-14 23:00:03 -0700

    updated

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """function to calculate operations."""
+    """performing operations on integers"""
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
-
+    print*"{} / {} = {}".format(a, b, div(a, b)))
