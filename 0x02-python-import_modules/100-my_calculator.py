255e0f1829f9e0dbde39b46f4a61e89045a4dcb8
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-16 04:10:44 -0700

    completed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-import calculator_1
 if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
@@ -9,13 +9,12 @@ if __name__ == "__main__":
     b = int(argv[3])
     op = argv[2]
     if op == "+":
-        print(f"{a} op {b} = {calculator_1.add(a, b)}")
-    elif op == "":
-        print(f"{a} op {b} = {calculator_1.sub(a, b)}")
-    elif op == "":
-        print(f"{a} op {b} = {calculator_1.mul(a, b)}")
-    elif op == "":
-        print(f"{a} op {b} = {calculator_1.div(a, b)}")
+        print(f"{a} + {b} = {add(a, b)}")
+    elif op == "-":
+        print(f"{a} + {b} = {sub(a, b)}")
+    elif op == "*":
+        print(f"{a} + {b} = {mul(a, b)}")
+    elif op == "/":
+        print(f"{a} + {b} = {div(a, b)}")
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
