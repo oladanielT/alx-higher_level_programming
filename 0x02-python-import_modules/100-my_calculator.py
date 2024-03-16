f60c5af462aebef1d797db8899213b82741b1601
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-16 03:54:15 -0700

    completed

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from sys import argv
+import calculator_1
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+    if op == "+":
+        print(f"{a} op {b} = {calculator_1.add(a, b)}")
+    elif op == "":
+        print(f"{a} op {b} = {calculator_1.sub(a, b)}")
+    elif op == "":
+        print(f"{a} op {b} = {calculator_1.mul(a, b)}")
+    elif op == "":
+        print(f"{a} op {b} = {calculator_1.div(a, b)}")
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
