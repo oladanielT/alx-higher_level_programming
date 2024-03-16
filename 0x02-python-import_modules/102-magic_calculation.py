ac38a457fbdf6f733010de2462ce370ace803387
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-16 06:17:01 -0700

    the Python function def magic_calculation(a, b)

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from magic_calculation_102 import a, b
+def magic_calculation(a, b):
+    if a < b:
+        c = a + b
+        for i in range(4, 6):
+            c += i
+    else:
+        c = a - b
+    return c
