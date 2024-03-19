471d13e0f014befb86496b03c2f9550d3fdefa12
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-19 02:08:59 -0700

    updated

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,3 +4,4 @@ def no_c(my_string):
         new_string = my_string.translate({ord('c') : None})
         gen_string = new_string.translate({ord('C') : None})
         return gen_string
+    return my_string
