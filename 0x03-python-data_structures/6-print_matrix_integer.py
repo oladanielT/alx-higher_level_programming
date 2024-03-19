3477031bc7a049237b77bf9fa95cd55c8f6cc2b9
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-19 05:48:12 -0700

    updated

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for row in matrix:
             for i, element in enumerate(row):
-                if i == len(row) - 1:
-                    print("{:d}".format(element))
-                else:
-                    print("{:d}".format(element), end=" ")
-    else:
-        return
+                print("{:d}".format(element), end=" " if i != len(row) - 1 else "")
+            print()
