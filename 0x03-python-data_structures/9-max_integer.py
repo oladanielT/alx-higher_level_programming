94ac9bfee0c262d6aa1d790f4a5b49f3f5c4370b
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-20 04:58:08 -0700

    function to print the maximum number of a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    else:
+        max_n = my_list[0]
+        for num in my_list:
+            if num > max_n:
+                max_n = num
+        return max_n
