3a8ffe5cff99b0cc55315a0d19116df35d0b25d2
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-20 04:24:17 -0700

    performing addition on tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a[0] if len(tuple_a) >= 1 else 0
+    b = tuple_a[1] if len(tuple_a) >= 2 else 0
+    c = tuple_b[0] if len(tuple_b) >= 1 else 0
+    d = tuple_b[1] if len(tuple_b) >= 2 else 0
+
+    sum_a = a + c
+    sum_b = b + d
+
+    return sum_a, sum_b
