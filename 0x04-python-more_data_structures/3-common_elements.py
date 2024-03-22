55c8ab945c033e652720d79395d29101ce70c17f
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-22 14:50:16 -0700

    updated

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for first in set_1:
-        for sec in set_2:
-            if first == sec:
-                return first
-            else:
-                return
+    if set_1 and set_2:
+        for i in set_1:
+            for j in set_2:
+                if i == j:
+                    return i
+                else:
+                    return None
+    else:
+        return
