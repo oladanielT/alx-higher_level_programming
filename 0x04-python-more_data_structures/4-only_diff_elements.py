84481c145483ea9f5418dc5bf48d00b4c3366e0e
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-03-23 04:02:27 -0700

    updated

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,10 @@
 #!/usr/bin/pyython3
 def only_diff_elements(set_1, set_2):
-    result = []
+    result = set()
     for i in set_1:
         if i not in set_2:
-            result.append(i)
+            result.add(i)
     for j in set_2:
         if j not in set_1:
-            result.append(j)
+            result.add(j)
     return result
