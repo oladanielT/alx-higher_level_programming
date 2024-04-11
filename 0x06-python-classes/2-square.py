e70e96403bf93f2084222d7d65e9b55ca4791a63
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-04-11 04:27:29 -0700

    update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """Define a square."""
-
 class Square:
     """Represent a square."""
     def __init__(self, size=0):
-        """
-        Initialize a square.
-
+        """Initialize a square.
         Args:
-            size (int): getting size of the square.
-
+            size (int): size of the square.
         Raises:
             TypeError: raise size must be an integer.
             ValueError: raise size must be greater than 0.
