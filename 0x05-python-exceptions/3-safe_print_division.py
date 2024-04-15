e10928abc39e0c08961d390e7c0ad6f5ad308939
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-04-15 09:29:57 -0700

    updated

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""Defines a division module"""
+
+
 def safe_print_division(a, b):
     """
     Divides two integers and prints the result.
@@ -15,7 +19,7 @@ def safe_print_division(a, b):
         result = None
     finally:
         if result is not None:
-            print("Inside result: {:.1f}".format(result))
+            print("Inside result : {:.1f}".format(result))
         else:
-            print("Inside result: None")
+            print("Inside result : None")
     return result
