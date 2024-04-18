c1d6924b199c3057f949a604fc52de87247ec819
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-04-18 04:06:42 -0700

    A  function that returns an object (Python data structure) represented by a JSON string:

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a function that return an object by aJSON string"""
+
+
+def from_json_string(my_str):
+    """
+    Represent a function that return object rep.
+    Paremeter:
+     my_str (str)
+    """
+    return json.loads(my_str)
