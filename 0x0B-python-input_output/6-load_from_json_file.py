b7e8969ff25646f3e35abc9da94d1d23cf525120
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-04-18 04:32:23 -0700

    function that creates an Object from a “JSON file”

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" function that creates an Object from a “JSON file”."""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    function that creates an Object from a JSON file.
+    Args:
+        filename (str).
+    """
+    with open(filename, "w") as file:
+        json.load(file)
