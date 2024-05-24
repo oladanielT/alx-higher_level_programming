d7c01f1fd23fb9fa1d79ac355f86852abbfecb80
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-05-24 12:21:47 -0700

    more queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,10 @@
 -- creating mysql server.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER
+	IF NOT EXISTS 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANTS PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO
+	'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
 
 FLUSH PRIVILEGES;
