2cf2b9675f38a82dcaa51b8658608225c3b4dc06
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-05-24 12:14:57 -0700

    more queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,8 @@
 -- creating mysql server.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd'
+IDENTIFIED BY 'user_0d_1_pwd';
+
 GRANTS PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
+
+FLUSH PRIVILEGES;
