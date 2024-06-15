4ebef8411f9fdb05427e1534f92970341f2b6517
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-06-15 03:25:52 -0700

    updated

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@
 def uppercase(str):
     for i in str:
         if (ord('a') <= ord(i) <= ord('z')):
-                s = chr(ord(i) - 32)
+            s = chr(ord(i) - 32)
         else:
             s = i
         print("{}".format(s), end="")
