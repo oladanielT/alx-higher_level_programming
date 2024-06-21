bf5d3dcf615805787aca64192ff4fba67f8c6e02
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-06-21 09:15:27 -0700

    updated

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,17 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-	    this.width = w;
-	    this.height = h;
-    }
-  }
+	constructor (w, h) {
+		if (w > 0 && h > 0) {
+			this.width = w;
+			this.height = h;
+		}
+	}
 
-  print () {
-    for (let i = 0; i <= this.height; i++) {
-      let line = '';
-      for (let j = 0; j <= this.width; j++) {
-        line += 'X';
-      }
-      console.log(line);
-    }
-  }
+	print () {
+		for (let i = 0; i < this.height; i++) {
+			console.log('X'.repeat(this.width));
+		}
+	}
 }
 module.exports = Rectangle;
