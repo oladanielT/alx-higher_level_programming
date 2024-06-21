6b2613a46d816211feee7a44a2f77e66bfa2d466
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-06-21 13:06:34 -0700

    class Square that defines a square and inherits from Square of 5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const BaseSquare = require('5-square.js');
+
+
+class Square extends BaseSquare {
+	constructor (size) {
+		super(size, size);
+	}
+
+	charPrint (c) {
+		for (let i = 0; i < this.height; i++) {
+			if (c === undefined) {
+				console.log('X'.repeat(this.width));
+			} else {
+				console.log(c.repeat(this.width));
+			}
+		}
+	}
+}
+
+module.exports = Square;
