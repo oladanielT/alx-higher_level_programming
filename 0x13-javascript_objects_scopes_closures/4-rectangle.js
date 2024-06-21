8c323925cd52b49606b1cdd8b63918db8c6e7b7c
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-06-21 12:44:28 -0700

    class Rectangle that defines a rectangle:

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -21,21 +21,8 @@ class Rectangle {
 		}
 
 		double () {
-			this.width *= 2;
-			this.height *= 2;
+			this.width = this.width * 2;
+			this.height = this.height * 2;
 		}
 }
-
-const r1 = new Rectangle(2, 3);
-console.log('Normal:');
-r1.print();
-
-console.log('Double:');
-r1.double();
-r1.print();
-
-console.log('Rotate:');
-r1.rotate();
-r1.print();
-
 module.exports = Rectangle;
