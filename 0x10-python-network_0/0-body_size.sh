b5de128a872bdc959a2d08826c88aebd84350057
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-07-05 15:35:32 -0700

    Updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
 
-if [ $# -ne 1 ]; then
-	exit 1
-fi
-
-URL=$1
-
-SIZE=$(curl -s "$URL" | wc -c)
-
-echo "$SIZE"
