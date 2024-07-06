40812cb67597d5fa0b91f36d2f52bc39f5b91afe
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-07-06 03:42:30 -0700

    Updated

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Bash script that sends a DELETE request to the URL passed as
-#the first argument and displays the body of the response
+#Bash to send a Delete request
 curl -sX "DELETE" "$1"
