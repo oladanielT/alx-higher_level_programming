06bf57dc060eb5e6ff9ffdd83c81c790bfc8bdde
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-07-06 06:37:02 -0700

    Script to display status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Bash script that sends a request to a URL passed as an argument
-#and displays only the status code
-curl -s -o /dev/null -w "%{http_code}" "$1"
+#Bash script to send request and display status code
+curl -s -0 /dev/null -w "%{http_code}" "$1"
