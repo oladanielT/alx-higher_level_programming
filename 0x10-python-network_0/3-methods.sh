33c46ae4e043c54b46475d16e128528298c361ea
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-07-06 04:30:55 -0700

    Updated

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Bash that display all HTTP methods the server will accept
-curl -s -I "$1" | grep "Allow" | cut -d " " -f2
+#Bash to display all HTTP methods
+curl -sI "$1" | grep "Allow" | cut -d " " -f -2
