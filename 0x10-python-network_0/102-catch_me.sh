398316d1bffd539326cd2983397cebd9ed7ad83c
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-07-07 03:02:11 -0700

    Updated

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Bash script to make a request
-curl -sL -X PUT "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+#Bash script to PUT and send message to the response.
+curl -s -X "You got me!" 0.0.0.0:5000/catch_me
