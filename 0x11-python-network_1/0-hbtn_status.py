171b25b282a049843a31ec807ef3d8407529fc9b
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-07-08 23:27:21 -0700

    Updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Function to fetch from a url"""
+"""
+function to fetch from
+https://alx-intranet.hbtn.io/status
+"""
+
+
 import urllib.request
 
 
-url = 'https://alx-intranet.hbtn.io/status'
-try:
-    with urllib.request.urlopen(url) as req:
-        response = req.read()
-        print("Body response:")
-        print("\t- type: ", type(response))
-        print("\t- content: ", response)
-        print("\t- utf8: ", response.decode('utf-8'))
-except urllib.error.Urlerror as er:
-    print(er.reason)
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+    body = res.read()
+    print("Body response:")
+    print("\t- type: ", type(body))
+    print("\t- content: ", body)
+    print("\t- utf8 content: ", body.decode('utf-8'))
