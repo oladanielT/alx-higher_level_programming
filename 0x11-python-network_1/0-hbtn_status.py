b73ff4dfe7e2a0ad7d5eddc458db58529b13c155
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-07-08 23:43:25 -0700

    Updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ import urllib.request
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     body_mess = response.read()
     print("Body response:")
-    print("\t- type: ", type(body_mess))
-    print("\t- content: ", body_mess)
-    print("\t- utf8: ", body_mess.decode('utf-8'))
+    print("\t- type: {}".format(type(body_mess)))
+    print("\t- content: {}".format(body_mess))
+    print("\t- utf8 content: {}".format(body_mess.decode('utf-8')))
