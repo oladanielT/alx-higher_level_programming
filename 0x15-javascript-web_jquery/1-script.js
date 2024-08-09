98d7f28213971024f8241437210a8e66660711c7
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-08-09 16:51:59 -0400

    JavaScript script that adds the class red to the <header> element when the user clicks on the tag DIV#red_header

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//     $('header').css('color', '#FF0000')
+// })
+
+$(document).bind('DOMContentLoaded', function() {
+    $('header').css('color', '#FF0000')
+})
