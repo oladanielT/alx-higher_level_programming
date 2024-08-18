e5c3dee29b6b86cb33e79305caa50bd5a680e1c9
Olasunkanmi Daniel Temitayo <dolasunkanmi@student.oauife.edu.ng>
2024-08-18 05:39:48 -0700

    Updated

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 const arg = Number(process.argv[2]);
-if (arg <= 0) {
-	return
-}
-if (isNaN) {
+if (isNaN(arg)) {
   console.log('Missing number of occurrences');
 } else if (arg > 0) {
-	for (let l = 0; l < arg; l++) {
-		console.log('C is fun');
-	}
+  for (let l = 0; l < arg; l++) {
+    console.log('C is fun');
+  }
 }
