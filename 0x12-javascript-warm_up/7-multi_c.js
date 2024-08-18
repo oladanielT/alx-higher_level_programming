196680e4cd035894459cbc4e347c3a93d1e72da4
Olasunkanmi Daniel Temitayo <dolasunkanmi@student.oauife.edu.ng>
2024-08-18 05:19:35 -0700

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const arg = Number(process.argv[2]);
+if (isNaN) {
+  console.log('Missing number of occurrences');
+}
+for (let l = 0; l < arg; l++) {
+  console.log('C is fun');
+}
